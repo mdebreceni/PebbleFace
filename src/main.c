987d3e8300f2a617f8700e440d29e20d4accb026
mdebreceni <mdebreceni@gmail.com>
2016-06-04 20:43:56 -0600

    Working now

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <pebble.h>
 enum {
-  KEY_TEMPERATURE = 0,
-  KEY_CONDITIONS
+    KEY_TEMPERATURE = 0,
+    KEY_CONDITIONS
 };
+
+// Store incoming information
+static char temperature_buffer[8];
+static char conditions_buffer[32];
+static char weather_layer_buffer[32];
 
 static Window *s_main_window;
 
@@ -18,19 +23,31 @@ static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    // Read tuples for data
+    Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
+    Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
+
+    // If all data is available, use it
+    if(temp_tuple && conditions_tuple) {
+        snprintf(temperature_buffer, sizeof(temperature_buffer), "%dF", (int)temp_tuple->value->int32);
+        snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
+    }
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_weather_layer, weather_layer_buffer);
 
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
 }
 
 static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
 }
 
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
 static void update_time() {
@@ -45,22 +62,34 @@ static void update_time() {
 
     static char s_datebuffer[11];
     strftime(s_datebuffer, sizeof(s_datebuffer), "%F", tick_time);
-    
+
     // Display this time on the TextLayer
     text_layer_set_text(s_time_layer, s_buffer);
     text_layer_set_text(s_date_layer, s_datebuffer);
-    
+
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     update_time();
+    // Get weather update every 30 minutes
+    if(tick_time->tm_min % 30 == 0) {
+        // Begin dictionary
+        DictionaryIterator *iter;
+        app_message_outbox_begin(&iter);
+
+        // Add a key-value pair
+        dict_write_uint8(iter, 0, 0);
+
+        // Send the message!
+        app_message_outbox_send();
+    }
 }
 
 static void main_window_load(Window *window) {
     // Get information about the Window
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
-    
+
     // Create GBitmap
     s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
 
@@ -72,8 +101,8 @@ static void main_window_load(Window *window) {
     layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
 
     // Create the TextLayers 
-    
-    
+
+
     // time layer
     s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
     text_layer_set_background_color(s_time_layer, GColorClear);
@@ -98,8 +127,8 @@ static void main_window_load(Window *window) {
     text_layer_set_background_color(s_weather_layer, GColorClear);
     text_layer_set_text_color(s_weather_layer, GColorWhite);
     text_layer_set_text_alignment(s_weather_layer, GTextAlignmentCenter);
-    
-    
+
+
     // Add it as a child layer to the Window's root layer
     layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
     layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
@@ -115,19 +144,19 @@ static void main_window_unload(Window *window) {
     // Destroy Date elements
     text_layer_destroy(s_date_layer);
     fonts_unload_custom_font(s_date_font);
-    
+
     // Destroy weather elements
     text_layer_destroy(s_weather_layer);
     fonts_unload_custom_font(s_weather_font);
-    
+
     // Destroy GBitmap
     gbitmap_destroy(s_background_bitmap);
 
     // Destroy BitmapLayer
     bitmap_layer_destroy(s_background_layer);
 
-    
-    
+
+
 }
 
 static void init() {
@@ -145,15 +174,15 @@ static void init() {
 
     // Register with TickTimerService
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-    
+
     window_set_background_color(s_main_window, GColorBlack);
-    
+
     // Register callbacks
     app_message_register_inbox_received(inbox_received_callback);
     app_message_register_inbox_dropped(inbox_dropped_callback);
     app_message_register_outbox_failed(outbox_failed_callback);
     app_message_register_outbox_sent(outbox_sent_callback);
-    
+
     // Open AppMessage
     const int inbox_size = 128;
     const int outbox_size = 128;
@@ -164,7 +193,7 @@ static void init() {
 static void deinit() {
     // Destroy Window
     window_destroy(s_main_window);
-    
+
 }
 
 
